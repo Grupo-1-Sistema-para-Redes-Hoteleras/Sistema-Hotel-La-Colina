0ea92019c9c3d9e650db015a648ef4b5cdd8ed71
Bernardino Gaona <83174066+BernardinoGaona@users.noreply.github.com>
2021-07-08 21:07:16 -0400

    Update Conexión.php

--- Backend/Tabla Trabajador/Conexión.php
+++ Backend/Tabla Trabajador/Conexión.php
@@ -1,33 +1,33 @@
-<? php
+<?php
 
-class  DB {
+class DB{
 
-     $ host privado ;
-    private  $ db ;
-     $ usuario privado ;
-     contraseña $ privada ;
-     juego de caracteres $ privado ;
+    private $host;
+    private $db;
+    private $user;
+    private $password;
+    private $charset;
 
-     función  pública __construct () {
-        $ esto -> host = 'localhost' ;
-        $ esto -> db = 'hotel' ;
-        $ esto -> usuario = 'root' ;
-        $ esto -> contraseña = '123qwe' ;
-        $ esto -> juego de caracteres = 'utf8mb4' ;
+    public function __construct(){
+        $this -> host = 'localhost';
+        $this -> db = 'hotel';
+        $this -> user = 'root';
+        $this -> password = '123qwe';
+        $this -> charset = 'utf8mb4';
     }
 
-    function  connect () {
-        prueba {
-            $ conexión = "mysql: host =" . $ this -> host . "; nombrebd =" . $ esto -> db . "; juego de caracteres =" . $ this -> juego de caracteres ;
+    function connect(){
+        try {
+            $connection = "mysql:host=" . $this->host . ";dbname=" . $this ->db. ";charset=" . $this->charset;
 
-            $ opciones = [ PDO :: ATTR_ERRMODE => PDO :: ERRMODE_EXCEPTION ,
-                        PDO :: ATTR_EMULATE_PREPARES => falso ,
-                        PDO :: MYSQL_ATTR_INIT_COMMAND => "ESTABLECER NOMBRES utf8mb4" ];
+            $options = [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, 
+                        PDO::ATTR_EMULATE_PREPARES => false,
+                        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"];
 
-            $ pdo = nuevo  PDO ( $ conexión , $ esto -> usuario , $ esto -> contraseña , $ opciones );
-            return  $ pdo ;
-        } captura ( PDOException  $ e ) {
-            print_r ( "Error de conexión:" . $ e -> getMessage ());
+            $pdo = new PDO($connection, $this->user, $this->password, $options);
+            return $pdo;
+        } catch (PDOException $e) {
+            print_r("Error connection: " . $e->getMessage());
             
         }
     }
